fd9fe9da91a4843cc2a76ebb28e4108ae941e762
Lucca Dias <lucca.gdias07@gmail.com>
2021-07-04 17:49:27 -0300

    Primeiro Commit

---
+++ src/test/java/com/zup/desafio/comicsapi/ComicsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.desafio.comicsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
